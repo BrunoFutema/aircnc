16219fefae871fa6c0954dd6fb9c256607d0617a
Bruno Futema <bruno.futema@outlook.com>
2020-12-21 20:31:14 -0300

    feat: :sparkles: Add Input component

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+
+  input {
+    width: 100%;
+    margin-bottom: 20px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    height: 45px;
+    padding: 0 15px;
+    font-size: 16px;
+  }
+`;

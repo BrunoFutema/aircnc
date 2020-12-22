08db8d5cf17d730b0ecaf8e849dea90d335ad6ea
Bruno Futema <bruno.futema@outlook.com>
2020-12-21 23:49:46 -0300

    chore: :wrench: add .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333/air-cnc',
+  baseURL: `${process.env.REACT_APP_API_URL}/air-cnc`,
 });
 
 export default api;

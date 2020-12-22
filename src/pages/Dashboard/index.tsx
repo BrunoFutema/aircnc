114672cb10b2c80d9f6a0ecbb1078b6009f1d08d
Bruno Futema <bruno.futema@outlook.com>
2020-12-21 21:36:32 -0300

    feat: :sparkles: Add Dashboard page

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,136 @@
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { format } from 'date-fns';
+import ptBr from 'date-fns/locale/pt-BR';
+import { Link } from 'react-router-dom';
+import { io } from 'socket.io-client';
+
+import Card from '../../components/Card';
+import Button from '../../components/Button';
+
+import api from '../../services/api';
+
+import {
+  Container,
+  Notifications,
+  Notification,
+  Spots,
+  Spot,
+  Thumbnail,
+} from './styles';
+
+interface ISpot {
+  id: string;
+  thumbnail: string;
+  company: string;
+  price: number;
+}
+
+interface INotification {
+  id: string;
+  user: { email: string };
+  spot: ISpot;
+  date: Date;
+  parsedDate: string;
+}
+
+const Dashboard: React.FC = () => {
+  const [spots, setSpots] = useState<ISpot[]>([]);
+  const [requests, setRequests] = useState<INotification[]>([]);
+
+  const user_id = localStorage.getItem('user');
+
+  const socket = useMemo(
+    () => io('http://localhost:3333', { query: { user_id } }),
+    [user_id],
+  );
+
+  const handleAccept = useCallback(
+    async id => {
+      await api.post(`/bookings/${id}/rejections`);
+
+      setRequests(requests.filter(request => request.id !== id));
+    },
+    [requests],
+  );
+
+  const handleReject = useCallback(
+    async id => {
+      await api.post(`/bookings/${id}/rejections`);
+
+      setRequests(requests.filter(request => request.id !== id));
+    },
+    [requests],
+  );
+
+  useEffect(() => {
+    socket.on('booking_request', (data: INotification) => {
+      data.parsedDate = format(new Date(data.date), "dd 'de' MMMM 'de' yyyy", {
+        locale: ptBr,
+      });
+
+      setRequests([...requests, data]);
+    });
+  }, [socket, requests]);
+
+  useEffect(() => {
+    async function loadSpots() {
+      const response = await api.get('/dashboard', { headers: { user_id } });
+
+      setSpots(response.data);
+    }
+
+    loadSpots();
+  }, [user_id]);
+
+  return (
+    <Container>
+      <Card>
+        <Notifications>
+          {requests.map(request => (
+            <Notification key={request.id}>
+              <p>
+                <strong>{request.user.email}</strong> está solicitando uma
+                reserva em <strong>{request.spot.company}</strong> para a data:{' '}
+                <strong>{request.parsedDate}</strong>
+              </p>
+
+              <button
+                type="button"
+                onClick={() => handleAccept(request.id)}
+                style={{ color: '#84C870' }}
+              >
+                ACEITAR
+              </button>
+
+              <button
+                type="button"
+                onClick={() => handleReject(request.id)}
+                style={{ color: '#E55E5E' }}
+              >
+                REJEITAR
+              </button>
+            </Notification>
+          ))}
+        </Notifications>
+
+        <Spots>
+          {spots.map(spot => (
+            <Spot key={spot.id}>
+              <Thumbnail thumb={spot.thumbnail} />
+
+              <strong>{spot.company}</strong>
+
+              <span>{spot.price ? `R$ ${spot.price}/dia` : 'Gratuito'}</span>
+            </Spot>
+          ))}
+        </Spots>
+
+        <Link to="/new">
+          <Button>Cadastrar novo spot</Button>
+        </Link>
+      </Card>
+    </Container>
+  );
+};
+
+export default Dashboard;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Notifications = styled.ul`
+  list-style: none;
+  margin-bottom: 15px;
+`;
+
+export const Notification = styled.li`
+  font-size: 16px;
+  line-height: 24px;
+
+  button {
+    margin-right: 10px;
+    border: 0;
+    background: none;
+    font-weight: bold;
+    margin-top: 10px;
+    cursor: pointer;
+  }
+`;
+
+export const Spots = styled.ul`
+  width: 100%;
+  list-style: none;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  margin-bottom: 30px;
+`;
+
+export const Spot = styled.li`
+  display: flex;
+  flex-direction: column;
+
+  strong {
+    margin-top: 10px;
+    font-size: 24px;
+    color: #444;
+  }
+
+  span {
+    font-size: 15px;
+    color: #999;
+  }
+`;
+
+interface ThumbnailProps {
+  thumb: string;
+}
+
+export const Thumbnail = styled.header<ThumbnailProps>`
+  width: 100%;
+  height: 120px;
+  background-size: cover;
+  border-radius: 4px;
+  background-image: ${props =>
+    `url(http://localhost:3333/air-cnc-files/${props.thumb})`};
+`;
